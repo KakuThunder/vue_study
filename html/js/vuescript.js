c5f008ab8c54fb2cb7e6e99e2f1adecc50899b60
kaku3 <pasuo1292r@gmail.com>
2020-01-25 12:47:44 +0900

    add html files

---
+++ html/js/vuescript.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+
+
+var vueapp = new Vue({
+    el:"#user",
+    data: {
+        name: "Fab"
+    }
+})
+
+var vuetask = new Vue({
+    el:"#todo", //紐づけ先
+    data: { //保持するデータ
+        tasks: [
+            {
+                name:"エキルレ",
+                status:true
+            },
+            {
+                name:"アラルレ",
+                status:false
+            },
+            {
+                name:"メインルレ",
+                status:false
+            }
+        ],
+        newitem:""
+    },
+    methods: { //持ちうる機能
+        addItem: function(){
+            let item = {
+                name:this.newitem,
+                status:false
+            }
+            this.tasks.push(item);
+            this.newitem = ''
+        },
+        deleteItem: function(index){
+            if (confirm('are you sure')){
+                this.tasks.splice(index, 1);
+            }
+        }
+    }
+})
+
+
+
+
+})();
