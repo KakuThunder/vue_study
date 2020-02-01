1df9b3aec38d3197f1d11bdc2eef15402a6170d0
kaku3 <pasuo1292r@gmail.com>
2020-02-01 19:30:32 +0900

    register component

---
+++ html/js/vuecomp.js
@@ -0,0 +1,47 @@
+(function() {
+
+'use strict';
+
+var likeComponent = Vue.extend({
+    props: {
+        message: {
+            type:String,
+            default: 'None'
+        }
+    },
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button @click="countUp">{{message}} {{count}}</button>',
+    methods: {
+        countUp: function() {
+            this.count ++;
+            this.$emit('increment');
+        }
+    }
+
+});
+
+
+var app = new Vue ({
+    el:"#app",
+    data: {
+        total:0
+    },
+    methods: {
+        incrementTotal: function(){
+            this.total++;
+        }
+    },
+    components: {
+        'like-component': likeComponent
+    }
+});
+
+
+
+
+
+})();
